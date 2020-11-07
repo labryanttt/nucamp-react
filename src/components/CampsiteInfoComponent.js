b76f43b14a69332e202cdeb1bdc07b4c5995eddb
Bryant Albert <labryanttt@yahoo.com>
2020-11-06 21:09:57 -0800

    Redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,8 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
+
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -32,7 +33,8 @@ function RenderComments({ comments }) {
                         </p>
                     </div>
                 ))}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+
             </div>
         );
     }
@@ -59,8 +61,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -144,7 +145,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
